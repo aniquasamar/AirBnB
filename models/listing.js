c25ab57e9d577eb34494c1581caddfc759ab3bfb
aniqua_samar <samar.aniqua021@gmail.com>
2025-10-15 22:40:49 +0530

    Added new route folder to remove bulkiness(listing and review routes) in app.js file using Express Router.

--- models/listing.js
+++ models/listing.js
@@ -10,9 +10,9 @@ const listingSchema = new Schema({
     description : String,
     image :{
         type : String,
-        default : "https://unsplash.com/photos/two-surfers-wait-for-waves-near-a-rocky-coastline-MA0nBwOmuwQ",
+        default : "https://images.unsplash.com/photo-1759854887818-0b505861a16d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=740",
         set : (v) => v ==="" 
-        ? "https://unsplash.com/photos/two-surfers-wait-for-waves-near-a-rocky-coastline-MA0nBwOmuwQ" 
+        ? "https://images.unsplash.com/photo-1759854887818-0b505861a16d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=740" 
         : v,
     },
     price : Number,

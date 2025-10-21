eba55d8e0d4e4747edef4b8c19fcb99ff4535e59
aniqua_samar <samar.aniqua021@gmail.com>
2025-10-21 19:22:10 +0530

    Restructured our listing, review and  user routes using MVC framework in new a Controller directory.

--- routes/review.js
+++ routes/review.js
@@ -1,34 +1,21 @@
 const express = require("express");
 const router = express.Router( { mergeParams: true} );
-const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/reviews.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middlewares.js");
 
+const reviewController = require("../controllers/review.js");
+
 //Review (POST Route)
-router.post("/" ,validateReview ,isLoggedIn, wrapAsync(async( req ,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    // console.log("new review saved");
-    // res.send("new review saved");
-    req.flash("success" ,"New Review Created!");
-    res.redirect(`/listings/${listing._id}`);
-
-}));
+router.post("/" ,
+    validateReview ,
+    isLoggedIn, 
+    wrapAsync(reviewController.createReview)
+);
 
 //Delete Review Route
-router.delete("/:reviewId" ,isLoggedIn, isReviewAuthor, wrapAsync(async (req , res) => {
-    let {id ,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id ,  { $pull: {reviews: reviewId} });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success" ,"Review Deleted!");
-    res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId" ,
+    isLoggedIn, isReviewAuthor, 
+    wrapAsync(reviewController.destroyReview)
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,56 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middlewares");
 
+const userController = require("../controllers/user");
 
-router.get("/signup" , (req, res) => {
-    res.render("users/signup.ejs");
-});
-
-router.post("/signup" ,wrapAsync(async(req, res) => {
-    try {
-        let {username ,email ,password} = req.body
-        const newUser = new User({email, username});
-        const registeredUser = await User.register(newUser, password);
-        req.login(registeredUser, (err) => {      //passport function
-            if(err) {
-                return next(err);
-            }
-            req.flash("success" ,"Welcome to Wanderlust");
-            res.redirect("/listings");
-        });
-    } catch(e) {
-        req.flash("error" ,e.message);
-        res.redirect("/signup");
-    }
-}));
-
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-})
-
-router.post("/login" ,saveRedirectUrl, passport.authenticate("local", {    //passport function
-    failureRedirect: "/login", 
-    failureFlash: true 
-    }) , 
-    async(req, res) => {
-        req.flash("success", "Welcome back to Wanderlust.");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-    }
+router.get("/signup" ,
+    userController.renderSignupForm 
 );
 
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {           //passport function
-        if(err) {
-            return next(err);
-        }
-        req.flash("success", "successfully logged out!");
-        res.redirect("/listings");
-    })
-});
+router.post("/signup" ,
+    wrapAsync(userController.signup)
+);
+
+router.get("/login", 
+    userController.renderLoginForm
+);
+
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local",
+        {    //passport function
+            failureRedirect: "/login",
+            failureFlash: true
+        }),
+    userController.login
+);
+
+router.get("/logout", 
+    userController.logout
+);
 
 module.exports = router;

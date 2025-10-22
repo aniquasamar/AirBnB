77b46213297845351204061a1b25010be3896093
aniqua_samar <samar.aniqua021@gmail.com>
2025-10-22 14:37:00 +0530

    Added uploading image option in edit form as well.And also there is preview of listing image in edit form.

--- routes/listing.js
+++ routes/listing.js
@@ -37,6 +37,7 @@ router.route("/:id")
     .put(
         isLoggedIn,
         isOwner,
+        upload.single("listing[image]"),
         validateListing,
         wrapAsync(listingController.updateListing)
     )
